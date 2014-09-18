cc3286f55260060a8f83bec5c64d5c2b14c09f8c
Javier Rotelli <javier.rotelli@gmail.com>
2014-09-18 15:01:18 -0300

    added model binding to the form

--- src/BootstrapForm.php
+++ src/BootstrapForm.php
@@ -99,10 +99,14 @@ class BootstrapForm
             $options['route'] = $options['store'];
             $options['method'] = 'post';
         }
+	
+	//bind the model to the form and remove it from the options
+	$this->form->model($options['model']);
+        array_forget($options, 'model');
 
         // Forget the routes provided to the input.
         array_forget($options, 'update');
-        array_forget($options, 'create');
+        array_forget($options, 'store');
 
         return $options;
     }
