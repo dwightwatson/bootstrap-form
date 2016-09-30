762ef03bbe6ec0ab3ef69f1b69f025690c2a039b
offical-mehdi <mohamad-mehdi@live.com>
2016-09-30 10:00:17 +0330

    fix config for right_column_class

--- src/config/config.php
+++ src/config/config.php
@@ -28,7 +28,7 @@ return [
     */
 
     'left_column_class'  => 'col-sm-2 col-md-3',
-    'right_column_class' => 'col-sm-3 col-md-9',
+    'right_column_class' => 'col-sm-8 col-md-9',
 
     'left_column_offset_class' => 'col-sm-offset-2 col-md-offset-3',
 
