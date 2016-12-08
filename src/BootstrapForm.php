e0f10e918228b0676bbc4fb72bf2f00d94df5bb8
hodaEdrob <hoda@edwardrobertson.co.uk>
2016-12-08 09:10:26 +0000

    Fix Radio Boxes with value of zero
    This is to check if the value of a radio box is zero to accept it as a value and not use the label for value, only use label for value when no value is set and the value is not zero

--- src/BootstrapForm.php
+++ src/BootstrapForm.php
@@ -446,7 +446,7 @@ class BootstrapForm
     {
         $label = $label === false ? null : $this->getLabelTitle($label, $name);
 
-        $value = $value ?: $label;
+        $value = !$value && $value != 0 ? $label : $value;
 
         $labelOptions = $inline ? ['class' => 'radio-inline'] : [];
 
