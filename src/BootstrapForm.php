7b14da097f601484f8c0ca89ac15bf4d6fcc9412
El Karima Réda <relkarima@utopiaimage.com>
2016-12-27 15:43:05 +0000

    just use the template blade parantheses without escape html

--- src/BootstrapForm.php
+++ src/BootstrapForm.php
@@ -4,6 +4,7 @@ namespace Watson\BootstrapForm;
 
 use Collective\Html\FormBuilder;
 use Collective\Html\HtmlBuilder;
+use Illuminate\Support\HtmlString;
 use Illuminate\Contracts\Config\Repository as Config;
 use Illuminate\Session\SessionManager as Session;
 use Illuminate\Support\Str;
@@ -73,6 +74,11 @@ class BootstrapForm
         $this->html = $html;
         $this->form = $form;
         $this->config = $config;
+    }
+
+    protected function toHtmlString($html)
+    {
+        return new HtmlString($html);
     }
 
     /**
@@ -650,7 +656,7 @@ class BootstrapForm
     {
         $options = $this->getFormGroupOptions($name);
 
-        return '<div' . $this->html->attributes($options) . '>' . $element . '</div>';
+        return $this->toHtmlString('<div' . $this->html->attributes($options) . '>' . $element . '</div>');
     }
 
     /**
@@ -665,7 +671,7 @@ class BootstrapForm
     {
         $options = $this->getFormGroupOptions($name);
 
-        return '<div' . $this->html->attributes($options) . '>' . $this->label($name, $value) . $element . '</div>';
+        return $this->toHtmlString('<div' . $this->html->attributes($options) . '>' . $this->label($name, $value) . $element . '</div>');
     }
 
     /**
@@ -919,7 +925,7 @@ class BootstrapForm
     protected function getHelpText($field, array $options = [])
     {
         if (array_key_exists('help_text', $options)) {
-            return '<span class="help-block">' . e($options['help_text']) . '</span>';
+            return $this->toHtmlString('<span class="help-block">' . e($options['help_text']) . '</span>');
         }
 
         return '';
