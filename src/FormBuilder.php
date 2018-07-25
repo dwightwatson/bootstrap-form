251921bee0e360055b79720f8fa7fd0207742721
Jérémy Gaulin <jeremy@bnb.re>
2018-07-25 11:39:15 +0400

    Fix default value

--- src/FormBuilder.php
+++ src/FormBuilder.php
@@ -32,8 +32,8 @@ class FormBuilder extends \Collective\Html\FormBuilder
 
     public function getValueAttribute($name, $value = null)
     {
-        if ($value === null && isset($this->defaultValues['name'])) {
-            $value = $this->defaultValues['name'];
+        if ($value === null && isset($this->defaultValues[$name])) {
+            $value = $this->defaultValues[$name];
         }
 
         return parent::getValueAttribute($name, $value);
